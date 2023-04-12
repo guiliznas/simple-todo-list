947ea895c1f700925d7b38d21a08f571ec0b5fd3
Guilherme de Liz <gliz@aawz.com.br>
2023-04-11 23:29:13 -0300

    feat: simple todo list initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    Simple To-do List
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="full-center">
+    <todo-list />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.full-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
+
---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive } from "vue";
+
+const tasks = reactive([
+  {
+    desc: "",
+    id: 1,
+  },
+  {
+    desc: "",
+    id: 2,
+  },
+  {
+    desc: "",
+    id: 3,
+  },
+  {
+    desc: "",
+    id: 4,
+  },
+  {
+    desc: "",
+    id: 5,
+  },
+]);
+</script>
+
+<template>
+  <div style="display: flex; flex-direction: column; gap: 8px;">
+    <template v-for="task in tasks" :key="task.id">
+      <input v-model="task.desc" placeholder="Tarefas" />
+    </template>
+  </div>
+</template>
